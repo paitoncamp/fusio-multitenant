95dbea1f1acf18f39ebfac6aa35d0a7a8c31087b
paitoncamp <paitoncamp@gmail.com>
2021-07-09 19:44:27 +0700

    clean up
    clean up update action

--- Action/Tenancy/Member/Update.php
+++ Action/Tenancy/Member/Update.php
@@ -1,60 +1,43 @@
 <?php
-/*
- * Fusio
- * A web-application to create dynamically RESTful APIs
- *
- * Copyright (C) 2015-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
-namespace Fusio\Impl\Backend\Action\User;
+namespace App\Action\Tenancy\Member;
 
 use Fusio\Engine\ActionAbstract;
 use Fusio\Engine\ContextInterface;
 use Fusio\Engine\ParametersInterface;
 use Fusio\Engine\RequestInterface;
 use Fusio\Impl\Authorization\UserContext;
-use Fusio\Impl\Service\User;
-use Fusio\Model\Backend\User_Update;
+//use Fusio\Impl\Service\User;
+use App\Service\Tenancy\TenantMember;
+//use Fusio\Model\Backend\User_Update;
+use App\Model\Tenancy\Member_Update;
 
 /**
  * Update
  *
- * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @author  wira m.s <Senasana.wira@gmail.com>
  * @license http://www.gnu.org/licenses/agpl-3.0
  * @link    http://fusio-project.org
  */
 class Update extends ActionAbstract
 {
     /**
-     * @var User
+     * @var TenantMember
      */
     private $userService;
 
-    public function __construct(User $userService)
+    public function __construct(TenantMember $tenantMemberService)
     {
-        $this->userService = $userService;
+        $this->tenantMemberService = $tenantMemberService;
     }
 
     public function handle(RequestInterface $request, ParametersInterface $configuration, ContextInterface $context)
     {
         $body = $request->getPayload();
 
-        assert($body instanceof User_Update);
+        assert($body instanceof Member_Update);
 
-        $this->userService->update(
+        $this->tenantMemberService->update(
             (int) $request->get('user_id'),
             $body,
             UserContext::newActionContext($context)
@@ -62,7 +45,7 @@ class Update extends ActionAbstract
 
         return [
             'success' => true,
-            'message' => 'User successful updated',
+            'message' => 'Member successful updated',
         ];
     }
 }
