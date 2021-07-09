401853637ba8c9508d70a2afc5d18b7397cf3929
paitoncamp <paitoncamp@gmail.com>
2021-07-09 21:00:42 +0700

    fix update action
    check current member id will be updated is the member of current owner

--- Action/Tenancy/Member/Update.php
+++ Action/Tenancy/Member/Update.php
@@ -11,6 +11,7 @@ use Fusio\Impl\Authorization\UserContext;
 use App\Service\Tenancy\TenantMember;
 //use Fusio\Model\Backend\User_Update;
 use App\Model\Tenancy\Member_Update;
+use PSX\Http\Exception as StatusCode;
 
 /**
  * Update
@@ -33,6 +34,33 @@ class Update extends ActionAbstract
 
     public function handle(RequestInterface $request, ParametersInterface $configuration, ContextInterface $context)
     {
+		$tenantId = $request->getHeader('tenantId');
+		$connection = $this->connector->getConnection('System');
+
+        $sql = "SELECT fusio_user.* ,
+				  (SELECT VALUE FROM fusio_user_attribute WHERE NAME='first_name' AND user_id=fusio_user.id) AS first_name,
+				  (SELECT VALUE FROM fusio_user_attribute WHERE NAME='last_name' AND user_id=fusio_user.id) AS last_name,
+				  (SELECT VALUE FROM fusio_user_attribute WHERE NAME='tenant_role' AND user_id=fusio_user.id) AS tenant_role
+				FROM fusio_user INNER JOIN 
+				(
+					SELECT USER_id FROM fusio_user_attribute WHERE VALUE=:tenant_id AND NAME='tenant_uid'
+				) members_id ON fusio_user.id=members_id.user_id
+				INNER JOIN 
+				(
+					SELECT USER_id FROM fusio_user_attribute WHERE VALUE='member' AND NAME='tenant_role'
+				) members_role ON members_id.user_id = members_role.user_id
+				WHERE members_id.user_id=:member_id and status<>0 
+                ORDER BY fusio_user.id ";
+
+        $member = $connection->fetchAssoc($sql, [
+            'member_id' => $request->get('member_id'),
+			'tenant_id' => $tenantId
+        ]);
+		//if current ID is memberId of current tenant, continue to update its data otherwise reject
+		if (empty($member)) {
+            throw new StatusCode\NotFoundException('Member is not available');
+        }
+		
         $body = $request->getPayload();
 
         assert($body instanceof Member_Update);
