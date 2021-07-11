3e61797c86eab8293ed2407974022769199b5c12
paitoncamp <paitoncamp@gmail.com>
2021-07-11 13:59:29 +0700

    Update TenantMember.php
    clean up
    set default member role to tenant_member_role_default  (config)

--- Service/Tenancy/TenantMember.php
+++ Service/Tenancy/TenantMember.php
@@ -1,25 +1,5 @@
 <?php
-/*
- * Fusio
- * A web-application to create dynamically RESTful APIs
- *
- * Copyright (C) 2015-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-namespace App\Service\Tenancy; //Fusio\Impl\Service\User;
+namespace App\Service\Tenancy;
 
 use Fusio\Impl\Authorization\UserContext;
 use Fusio\Engine\ContextInterface;
@@ -28,9 +8,7 @@ use Fusio\Impl\Table;
 use Fusio\Model\Backend\User_Create;
 use Fusio\Model\Backend\User_Update;
 use Fusio\Model\Backend\User_Attributes;
-//use Fusio\Model\Consumer\User_Register;
 use App\Model\Tenancy\Member_Create;
-//use PSX\Framework\Util\Uuid;
 use PSX\Http\Exception as StatusCode;
 use PSX\Sql\Condition;
 
@@ -38,8 +16,6 @@ use PSX\Sql\Condition;
  * TenantRegister
  * Extend Register Service capability
  * @author  Wira m.s <senasana.wira@gmail.com>
- * @license http://www.gnu.org/licenses/agpl-3.0
- * @link    http://fusio-project.org
  */
 class TenantMember
 {
@@ -96,8 +72,8 @@ class TenantMember
         $this->mailerService  = $mailerService;
         $this->configService  = $configService;
         $this->roleTable      = $roleTable;
-		$this->tenantOwnerTable      = $tenantOwnerTable;
-		$this->tenantOwnerAttrTable = $tenantOwnerAttrTable;
+	$this->tenantOwnerTable      = $tenantOwnerTable;
+	$this->tenantOwnerAttrTable = $tenantOwnerAttrTable;
     }
 
     public function create(Member_Create $member, ContextInterface $context)
@@ -111,7 +87,7 @@ class TenantMember
         }
 
         $condition = new Condition();
-        $condition->equals('name', $this->configService->getValue('role_default'));
+        $condition->equals('name', $this->configService->getValue('tenant_member_role_default'));
         $role = $this->roleTable->getOneBy($condition);
         if (empty($role)) {
             throw new StatusCode\InternalServerErrorException('Invalid default role configured');
@@ -124,34 +100,29 @@ class TenantMember
         $user->setEmail($member->getEmail());
         $user->setPassword($member->getPassword());
 		
-		//--search current tenant owner data 
-		$tenant = $context->getUser();
-		$condThisTenant = new Condition();
-		$condThisTenant->equals('id', $tenant->getId());
-		$tenantOwner = $this->tenantOwnerTable->getOneBy($condThisTenant);
-		$condThisTenantAttr = new Condition();
-		$condThisTenantAttr->equals('user_id',$tenant->getId());
-		$condThisTenantAttr->equals('name','tenant_uid');
-		
-		$tenantOwnerAttr = $this->tenantOwnerAttrTable->getOneBy($condThisTenantAttr);
-		//$Uuid = new Uuid();
-		//$userAttrs = array('tenant_uid'=>$Uuid->pseudoRandom(),'tenant_role'=>'owner');
-		
-		$userAttrs = new User_Attributes();
-		$userAttrs->setProperties(array('tenant_uid'=>$tenantOwnerAttr['value'],'tenant_role'=>'member'));
-		
-		//$user->setAttributes($userAttrs);
-		
-		$userUpd = new User_Update();
-		$userUpd->setRoleId($user->getRoleId());
-		$userUpd->setStatus($user->getStatus());
-		$userUpd->setName($user->getName());
-		$userUpd->setEmail($user->getEmail());
-		$userUpd->setAttributes($userAttrs);
+	//--search current tenant owner data 
+	$tenant = $context->getUser();
+	$condThisTenant = new Condition();
+	$condThisTenant->equals('id', $tenant->getId());
+	$tenantOwner = $this->tenantOwnerTable->getOneBy($condThisTenant);
+	$condThisTenantAttr = new Condition();
+	$condThisTenantAttr->equals('user_id',$tenant->getId());
+	$condThisTenantAttr->equals('name','tenant_uid');
+
+	$tenantOwnerAttr = $this->tenantOwnerAttrTable->getOneBy($condThisTenantAttr);
+	$userAttrs = new User_Attributes();
+	$userAttrs->setProperties(array('tenant_uid'=>$tenantOwnerAttr['value'],'tenant_role'=>'member'));
+
+	$userUpd = new User_Update();
+	$userUpd->setRoleId($user->getRoleId());
+	$userUpd->setStatus($user->getStatus());
+	$userUpd->setName($user->getName());
+	$userUpd->setEmail($user->getEmail());
+	$userUpd->setAttributes($userAttrs);
 
         $userId = $this->userService->create($user, UserContext::newActionContext($context));
-		//-- soon, update the current new user
-		if($userId)	$this->userService->update($userId,$userUpd,UserContext::newActionContext($context));
+	//-- soon, update the current new user
+	if($userId)	$this->userService->update($userId,$userUpd,UserContext::newActionContext($context));
 		
         // send activation mail
         if ($approval) {
@@ -161,11 +132,4 @@ class TenantMember
         }
     }
 	
-	public function update(){
-		//-- TO DO
-	}
-	
-	public function delete($userId, UserContext $context){
-		//-- TO DO
-	}
 }
